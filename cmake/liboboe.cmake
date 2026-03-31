ca25539cf7e768fdd2a3b2e268c3a4b8c7c43152
Nicolas Gry <grybouilli@outlook.fr>
2026-03-31 12:00:03 +0200

    feat: fetch liboboe dependency

---
+++ cmake/liboboe.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+
+# ── Download & extract ─────────────────────────────────────────────────────────
+FetchContent_Declare(
+    oboe
+    GIT_REPOSITORY https://github.com/google/oboe
+    GIT_TAG 1.9.0 # crash happens with 1.10.0
+)
+
+FetchContent_MakeAvailable(oboe)
